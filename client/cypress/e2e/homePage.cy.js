5de42e9125169e5349ba69c7d648b74fdde321f7
Adam Dubey <adubey0311@gmail.com>
2022-08-18 10:24:55 -0400

    [AD] Refactoring basic smoke test for ci pipeline

--- client/cypress/e2e/homePage.cy.js
+++ client/cypress/e2e/homePage.cy.js
@@ -1,5 +1,8 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io');
+describe('homePage loads on visit', () => {
+  it('displays homePage header', () => {
+    cy.visit('http://localhost:3000');
+    cy.get('h1');
+    cy.should('be.visible');
+    cy.should('have.text', 'Home Page');
   });
 });
