c008a864852c966959c82f986dd94e3c804023d8
Adam Dubey <adubey0311@gmail.com>
2022-08-25 14:50:44 -0400

    [AD] Refactor text editor in client-app

--- client/pages/admin/posts/index.js
+++ client/pages/admin/posts/index.js
@@ -8,9 +8,16 @@ function Post() {
     <AdminLayout>
       <Row>
         <Col span={24}>
-            <Button type="primary"><Link href="/admin/posts/new"><a><PlusOutlined />Add New</a></Link></Button>
-            <h1>Posts</h1>
-            <p>...</p>
+          <Button type="primary">
+            <Link href="/admin/posts/new">
+              <a>
+                <PlusOutlined />
+                Add New
+              </a>
+            </Link>
+          </Button>
+          <h1>Posts</h1>
+          <p>...</p>
         </Col>
       </Row>
     </AdminLayout>
--- client/pages/admin/posts/new.js
+++ client/pages/admin/posts/new.js
@@ -1,27 +1,93 @@
-import { useContext, useState } from "react";
-import { Col, Layout, Row } from 'antd';
+import { useContext, useEffect, useState } from 'react';
+import { Col, Input, Layout, Row } from 'antd';
 import AdminLayout from '../../../components/layout/AdminLayout';
-import Editor from "rich-markdown-editor";
-import { ThemeContext } from "../../../context/theme";
+import Editor from 'rich-markdown-editor';
+import { ThemeContext } from '../../../context/theme';
+import axios from 'axios';
 
-const { Content, Sider } = Layout; // eslint-disable-line no-unused-vars
+const { Option } = Select;
+const { Content, Sider } = Layout;
 
 function NewPost() {
+  // load from localStorage
+  const savedTitle = () => {
+    if(process.browser) {
+      if(localStorage.getItem("post-title")) {
+        return JSON.parse(localStorage.getItem("post-title"));
+      }
+    }
+  }
+
+  const savedContent = () => {
+    if(process.browser) {
+      if(localStorage.getItem("post-content")) {
+        return JSON.parse(localStorage.getItem("post-content"));
+      }
+    }
+  }
+
   // context
   const [theme, setTheme] = useContext(ThemeContext);
 
   // state
-  const [content, setContent] = useState("");
+  const [content, setContent] = useState(savedContent());
+  const [title, setTitle] = useState(savedTitle());
+  const [categories, setCategories] = useState([]);
+  const [loadedCategories, setLoadedCategories] = useState([]);
+
+  useEffect(() => {
+    loadCategories();
+  }, [])
+  
+  const loadCategories = async () => {
+    try {
+      const { data } = await axios.get("/categories");
+      setLoadedCategories(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   return (
     <AdminLayout>
       <Row>
         <Col span={14} offset={1}>
           <h1>Create New Post</h1>
-          <Editor dark={theme === 'light' ? false : true} onChange={(v) => setContent(v())} />
+          <Input
+            size="large"
+            placeholder="Give your post a title"
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+              localStorage.setItem('post-title', JSON.stringify(e.target.value));
+            }}
+          />
+          <br />
+          <br />
+          <Editor
+            defaultValue={content}
+            dark={theme === 'light' ? false : true}
+            onChange={(v) => {
+              setContent(v());
+              localStorage.setItem('post-content', JSON.stringify(v()));
+            }}
+            uploadImage={(file) => console.log(file)}
+          />
+          <br />
+          <br />
+
         </Col>
         <Col span={6} offset={1}>
-          Sidebar
+          <h4>Categories</h4>
+          <Select 
+            mode="multiple"
+            allowClear={true}
+            placeholder="Select Categories"
+            style={{ width: '100%' }}
+            onChange={(v) => setCategories(v)}
+          >
+            {loadedCategories.map((item) => (<Option key={item.name}>{item.name}</Option>)}
+          </Select>
         </Col>
       </Row>
     </AdminLayout>
