ef35a2a2437f1ee7111612c7b1015fa347aab5b3
Adam Dubey <adubey0311@gmail.com>
2022-08-29 15:22:44 -0400

    [AD] Adding routes and controllers for posts

---
+++ server/controllers/post.js
@@ -0,0 +1,17 @@
+import cloudinary from 'cloudinary';
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
+});
+
+export const uploadImage = async (req, res) => {
+    try {
+        const result = await cloudinary.uploader.upload(req.body.image);
+
+        res.json(result.secure_url)
+    } catch (err) {
+        console.log(err);
+    }
+}
