bfc89e58eebb404b3f287dd943c04b40e60c7594
Adam Dubey <adubey0311@gmail.com>
2022-09-14 16:30:38 -0400

    [AD] Refactor middlewares and integrate author rbac dashboard in server-app

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 import expressJwt from 'express-jwt';
 import User from '../models/user';
+import Post from '../models/post';
+import Media from '../models/media';
 
 export const requireSignin = expressJwt({
   secret: process.env.JWT_SECRET,
@@ -27,6 +29,79 @@ export const isAuthor = async (req, res, next) => {
       return res.status(403).send({ error: 'Access denied!' });
     } else {
       next();
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const canCreateRead = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id);
+
+    switch (user.role) {
+      case 'Admin':
+        next();
+        break;
+
+      case 'Author':
+        next();
+        break;
+
+      default:
+        return res.status(403).send('unauthorized');
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const canUpdateDeletePost = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id);
+    const post = await Post.findById(req.params.postId);
+
+    switch (user.role) {
+      case 'Admin':
+        next();
+        break;
+
+      case 'Author':
+        if (post.postedBy.toString() !== user._id.toString()) {
+          return res.status(403).send('unauthorized');
+        } else {
+          next();
+        }
+        break;
+
+      default:
+        return res.status(403).send('unauthorized');
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const canDeleteMedia = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id);
+    const media = await Media.findById(req.params.id);
+
+    switch (user.role) {
+      case 'Admin':
+        next();
+        break;
+
+      case 'Author':
+        if (media.postedBy.toString() !== user._id.toString()) {
+          return res.status(403).send('unauthorized');
+        } else {
+          next();
+        }
+        break;
+
+      default:
+        return res.status(403).send('unauthorized');
     }
   } catch (err) {
     console.log(err);
