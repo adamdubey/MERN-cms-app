3f03b5c5292fad8a4743cda4998bca89b37fbc1d
Adam Dubey <adubey0311@gmail.com>
2022-09-15 13:51:53 -0400

    [AD] Adding subscriber-role navigations and layout for client-app

---
+++ client/pages/subscriber/index.js
@@ -0,0 +1,11 @@
+import SubscriberLayout from '../../components/layout/SubscriberLayout';
+
+function Subscriber() {
+  return (
+    <SubscriberLayout>
+      <h1>Subscriber Page</h1>
+    </SubscriberLayout>
+  );
+}
+
+export default Subscriber;
